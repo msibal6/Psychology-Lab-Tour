cd88aedc52814375d373a0681aba37903b16cf56
Mitchell Sibal <mitchell.sibal@gmail.com>
2019-11-16 10:00:28 -0800

    First push with project files

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    /*
+     *
+     */
+
+    public float horizontalPan = 2.5f;
+    public float verticaPan = 2.5f;
+    public float moveSpeed = 2.0f; 
+
+    private float yaw;
+    private float pitch;
+
+    private float maxVertical = 30f;
+    private float maxHorizontal = 30f;
+    private Camera cam;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += horizontalPan * Input.GetAxis("Mouse X");
+        pitch -= verticaPan * Input.GetAxis("Mouse Y");
+
+
+        transform.position += moveSpeed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        transform.eulerAngles = new Vector3(Mathf.Clamp(pitch,-maxVertical,maxVertical), Mathf.Clamp(yaw,-maxHorizontal,maxHorizontal));
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("Hit the sphere");
+            Debug.Log(hit.transform);
+        }
+
+
+    }
+}
