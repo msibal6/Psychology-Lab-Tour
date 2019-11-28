1068d9e349c9713230d62f542c154978a9a0b2c7
Mitchell Sibal <mitchell.sibal@gmail.com>
2019-11-27 19:59:36 -0800

    Grabbing can now pickup and drop objects

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b126f7bdac922a7dd8845d5c637460da4451579468417f17ee6bf007c70c48a
-size 2428
+oid sha256:4b9641a28112b29ab1a0a2f8255fd4bacd411a897587c112ff85566958c2e778
+size 2626
