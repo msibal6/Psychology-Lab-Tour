fb35f8c51ea6386fc7e46d34332786f8700bbfcd
Mitchell Sibal <mitchell.sibal@gmail.com>
2019-12-02 15:06:39 -0800

    Introduced Models

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b9641a28112b29ab1a0a2f8255fd4bacd411a897587c112ff85566958c2e778
-size 2626
+oid sha256:7fc2ba563c740fda6794c079b3c927c492d0a84106542ce6af9e1e0ff74b728a
+size 2664
