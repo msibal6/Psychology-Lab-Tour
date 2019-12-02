72096bf78de57bf2ae9ed848e94b6b30b4d97683
Mitchell Sibal <mitchell.sibal@gmail.com>
2019-12-02 15:59:56 -0800

    Adding player following to grabbed object

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7fc2ba563c740fda6794c079b3c927c492d0a84106542ce6af9e1e0ff74b728a
-size 2664
+oid sha256:3bec0a4c3fa27f8633406e30ca43950d55bdc5a8d15b747de279fa5e1befb480
+size 2775
