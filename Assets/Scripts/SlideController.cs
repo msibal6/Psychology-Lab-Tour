4950fbe119c8ce4541c3a52eaa43d1b5c6cb2de1
Mitchell Sibal <mitchell.sibal@gmail.com>
2019-12-19 17:35:07 -0800

    Can now put slides on the microscope

---
+++ Assets/Scripts/SlideController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideController : MonoBehaviour
+{
+
+
+    private GameObject holder;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool IsHeld()
+    {
+        return (holder != null);
+
+
+    }
+
+    public GameObject GetHolder()
+    {
+
+        return holder;
+
+    }
+
+    public void SetHolder(GameObject newHolder)
+    {
+
+      
+        holder = newHolder;
+
+    }
+
+
+}
