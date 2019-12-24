9afd432ec101364429ace6443aab3fbb2dbe2542
Mitchell Sibal <mitchell.sibal@gmail.com>
2019-12-23 21:34:12 -0800

    Added sceneManager to handle switching between different views

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21d09204fed5b334c015a8b4182f0e6db34c0691fe483ed266d8ff81b45c1f57
-size 4897
+oid sha256:2bad93a6a916a056713b96862dd152f7d95b64278837bae6b8fed672e086662b
+size 5268
---
+++ Assets/Scripts/MySceneManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2835069779ef5606f28000da65e4eb43d564dbbd172f83099d9a1cf3910b26ff
+size 786
