8337d7d26a91b9b6d0a375863edd09fc04ccefb7
Kelly Chan <kellychan@kellys-mbp.attlocal.net>
2020-01-06 08:09:38 -0800

    new changes to ui

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1351136741acc51fc3783436ef4e5fbc794e95dc381281f8bb97389f7c6c2f5c
+size 257
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c9e8ec6862e0b5c7c40f325ea2d03d5794408aca771960057b3064a451885ce
+size 293
