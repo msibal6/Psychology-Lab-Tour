c9438dc7de268b47674f2d89577e0321723e663e
Kelly Chan <kellychan@campus-116-222.ucdavis.edu>
2020-01-31 12:49:29 -0800

    1/30/2020

---
+++ Assets/Scripts/ChangeSlides.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ChangeSlides : MonoBehaviour
+{
+	public Sprite[] s1;
+	public Button b1; 
+
+	int count = 0;
+
+	void Awake()
+	{
+		s1 = Resources.LoadAll<Sprite>("Sprites");
+	}
+
+	public void on_Click_Button(){
+		count++;
+
+		if (count== s1.Length) {
+			count = 0;
+			}
+
+		b1.image.sprite = s1[count];
+		
+
+	}
+}
